a8a6d2313e0961cf6680a008de4940e1a7cca039
Josue Rodriguez <josavidev@gmail.com>
2022-09-23 01:36:29 -0500

    Telephone Number Validator challenge completed!

--- TelephoneNumberValidator.js
+++ TelephoneNumberValidator.js
@@ -1,7 +1,18 @@
 "use strict";
 
 function telephoneCheck(str) {
-  return true;
+  const countryCodeFormat = /^1\s?/;
+  const USPhoneFormats = [
+    /^\d{3}-\d{3}-\d{4}$/g,
+    /^\(\d{3}\)\d{3}-\d{4}$/g,
+    /^\(\d{3}\)\s\d{3}-\d{4}$/g,
+    /^\d{3}\s\d{3}\s\d{4}$/g,
+    /^\d{10}$/g,
+    /^\d{3}\s\d{3}\s\d{4}$/g
+  ];
+  const phoneNumber = str.replace(countryCodeFormat, "");
+  const results = USPhoneFormats.filter(format => format.test(phoneNumber));
+  return results.length > 0;
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("555-555-5555"));
