3e6265ca45234c603d820d387d51abd730ad3ba4
Josue Rodriguez <josavidev@gmail.com>
2022-09-23 01:09:51 -0500

    Roman Numeral Checker challenge completed!

--- RomanNumeralChecker.js
+++ RomanNumeralChecker.js
@@ -1,7 +1,21 @@
 "use strict";
 
 function convertToRoman(num) {
-  return num;
+  let temp = num;
+  let result = "";
+  const ROMAN_NUMBERS = {
+    "M": 1000, "CM": 900, "D": 500, "CD": 400,
+    "C": 100, "XC": 90, "L": 50, "XL": 40,
+    "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1
+  };
+  Object.keys(ROMAN_NUMBERS).forEach(key => {
+    const qoutient = Math.floor(temp / ROMAN_NUMBERS[key]);
+    temp -= ROMAN_NUMBERS[key] * qoutient;
+    for (let i = 0; i < qoutient; i++) {
+      result += key;
+    }
+  });
+  return result;
 }
 
-convertToRoman(36);
+console.log(convertToRoman(36));
